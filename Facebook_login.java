dd3c2ef340fac87b6edac97ab894cbf54faa300b
Sambit Mohapatra <sambit855@gmail.com>
2025-09-18 03:19:08 +0530

    Add files via upload

---
+++ Facebook_login.java
@@ -0,0 +1,36 @@
+package playwright;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class Facebook_login {
+
+	public static void main(String[] args) {
+
+Playwright playwright = Playwright.create();
+Browser browser =playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+Page page = browser.newPage();
+page.navigate("https://www.facebook.com/");
+String title = page.title();
+System.out.println("page title is |:" +title);
+
+
+page.locator("id=email").click();
+page.locator("id=email").fill("sambit85@gmail.com");
+page.locator("id=pass").click();
+page.locator("id=pass").fill("gsds#42jshds");
+
+page.locator("button[name=login]").click();
+browser.close();
+playwright.close();
+
+
+
+
+
+
+	}
+
+}
